94c94762f1253ef8e19f2f9fd4131ed7613ba13c
lisandroviedo <lisandroviedo@hotmail.com>
2023-10-14 22:43:18 -0400

    M2 14/10/2023

--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -3,7 +3,7 @@ const contador = require("./reducer");
 const { incremento, decremento } = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+var store = createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
 var valor;
@@ -13,12 +13,13 @@ var valor;
 // Utilizamos el elemento obtenido arriba para mostrar el State.
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
+  return console.log(store.getState());
   // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
 }
 
 // Ejecutamos la función 'renderContador':
-
+renderContador();
 // Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
-
+store.subscribe(renderContador());
 // Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la acción correspondiente:
